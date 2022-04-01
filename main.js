d62d07d773dd5740e6d665336a12197f42840c2f
Lilian Forger <lilianforger@gmail.com>
2022-04-01 11:33:43 +0200

    Fixed responsiveness for screens 400px and smaller

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 const btn = document.querySelector('.btn');
-const burgerMenu = document.querySelector('.navbar-toggler');
 
 const popupMessage = () => alert('Hello!');
 
 btn.addEventListener('click', popupMessage);
-burgerMenu.addEventListener('click', popupMessage);
 
 // DOM manipulation to select all buttons in the screen
 //const btn = document.querySelectorAll('.btn').forEach(btn => btn.onclick = () => alert('Hello!'));
